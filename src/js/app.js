9c06b6b9fe326e664c0e3cab0ad2e0090fdc23af
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-02-06 12:10:39 +0100

    Declare DOM elements in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,33 @@
+
+
 //BUTTONS
 const addItemButton = document.querySelector(".add-and-search__addItem");
 const closeOverlayButton = document.querySelector(".close-overlay-button");
+const saveButton = document.querySelector(".save-button");
+const cancleButton = document.querySelector(".cancel-button");
 
 // CONTAINERS
 const editPage = document.querySelector(".edit-page");
+const listContainer = document.querySelector(".list");
+
+// INPUTS
+const nameInput = document.querySelector(".name");
+const manufacturerInput = document.querySelector(".manufacturer");
+const idInput = document.querySelector(".id");
+const expirationInput = document.querySelector(".expiration");
+const stockInput = document.querySelector(".stock");
+
+const searchInput = document.querySelector(".search");
 
 // EVENT LISTENERS
-
 addItemButton.addEventListener("click", ()=> {
 	editPage.style.display = "block";
-})
+	listContainer.style.display = "none"
+
+});
 
 closeOverlayButton.addEventListener("click", ()=> {
 	editPage.style.display = "none";
-})
+	listContainer.style.display = "flex";
+});
 
---
+++ src/js/medicine.js
@@ -0,0 +1,11 @@
+export class Medicine {
+	constructor(name, manufacturer, id, expiration, qty){
+		this.name = name
+		this.manufacturer = manufacturer
+		this.id = id
+		this.expiration = expiration
+		this.qty = qty
+	}
+
+	
+}
